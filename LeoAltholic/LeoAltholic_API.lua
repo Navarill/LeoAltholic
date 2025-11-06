834a577d19621ca5fa8509a8ed66a70d0467c73e
Navarill <6269482+Navarill@users.noreply.github.com>
2025-11-05 23:14:46 -0600

    Fix daily reset time for craft writs

--- LeoAltholic/LeoAltholic_API.lua
+++ LeoAltholic/LeoAltholic_API.lua
@@ -9,8 +9,8 @@ end
 Return the timestamp from today's reset (specifically for craft writs)
 ]]
 function LeoAltholic.TodayReset()
-    local diff = zo_floor(GetDiffBetweenTimeStamps(GetTimeStamp(), 1538200800) / 86400)
-    return 1538200800 + (diff * 86400)
+    local diff = zo_floor(GetDiffBetweenTimeStamps(GetTimeStamp(), 1538190000) / 86400)
+    return 1538190000 + (diff * 86400)
 end
 
 --[[
